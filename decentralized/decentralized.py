c08f988e65dc685bf36d624c5ce9a487a5436dc8
Brandon Pae <>
2024-12-07 11:41:18 -0500

    added other files to test

--- decentralized/decentralized.py
+++ decentralized/decentralized.py
@@ -1,4 +1,5 @@
 import velocity_obstacle.velocity_obstacle as velocity_obstacle
+import kkt.kkt_solver as kkt_solver
 import nmpc.nmpc as nmpc
 import argparse
 
@@ -10,9 +11,11 @@ if __name__ == "__main__":
         "-f", "--filename", help="filename, in case you want to save the animation")
 
     args = parser.parse_args()
-    if args.mode == "velocity_obstacle":
+    if args.mode == "velocity_obstacle": # existing method
         velocity_obstacle.simulate(args.filename)
-    elif args.mode == "nmpc":
+    elif args.mode == "nmpc":  # existing method
         nmpc.simulate(args.filename)
+    elif args.mode == "kkt": # our method
+        kkt_solver.simulate(args.filename)
     else:
         print("Please enter mode the desired mode: velocity_obstacle or nmpc")
--- decentralized/utils/multi_robot_plot.py
+++ decentralized/utils/multi_robot_plot.py
@@ -48,12 +48,37 @@ def plot_robot_and_obstacles(robot, obstacles, robot_radius, num_steps, sim_time
     # Save animation
     if not filename:
         return
-
+    
+    # Reduced fps for slower playback
+    FPS = 10  # Changed from 30 to 10
+    
     ani = animation.FuncAnimation(
-        fig, animate, np.arange(1, num_steps), interval=200,
-        blit=True, init_func=init)
+        fig, animate, np.arange(1, num_steps), 
+        interval=1000/FPS,  # interval in milliseconds (1000ms/fps)
+        blit=True, 
+        init_func=init)
 
-    ani.save(filename, "ffmpeg", fps=30)
+    # Get file extension
+    file_extension = filename.split('.')[-1].lower()
+    
+    if file_extension == 'mp4':
+        # Save as MP4
+        writer = animation.FFMpegWriter(
+            fps=FPS,  # Reduced fps
+            metadata=dict(artist='Me'),
+            bitrate=1800
+        )
+        ani.save(filename, writer=writer)
+    
+    elif file_extension == 'gif':
+        # Save as GIF
+        ani.save(filename, writer='pillow', fps=FPS)  # Reduced fps
+    
+    else:
+        print(f"Unsupported file format: {file_extension}")
+        print("Please use .mp4 or .gif extension")
+    
+    plt.close()  # Close the figure to free memory
 
 
 def plot_robot(robot, timestep, radius=1, is_obstacle=False):
